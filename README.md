ce1e07df915c68b90872417abb4fa26b6e598401
chaitanyareddyk <chaitanyareddy.kothapeta@gmail.com>
2020-08-19 12:14:30 +0530

    base code

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# @chaitanyareddy/react-password-strength
+# @chaitanyareddy/simple-password-strength
 
+![npm (scoped)](https://img.shields.io/npm/v/@chaitanyareddy/simple-password-strength)
+
+Get password strength in text/ color/ colored text or simple numerical score
+
+## Installation
+
+```
+$ npm install @chaitanyareddy/simple-password-strength
+```
+
+## Usage
+
+### Get password strength's text:
+
+```js
+const {strengthScoreText} = require("@chaitanyareddy/simple-password-strength");
+
+const passwordStrengthText = strengthScoreText("Pass your password here!");
+console.log(passwordStrengthText)
+```
+
+### Get password strength's color:
+
+```js
+const {strengthScoreColor} = require("@chaitanyareddy/simple-password-strength");
+
+const passwordStrengthColor = strengthScoreColor("Pass your password here!");
+console.log(passwordStrengthColor)
+```
+
+### Get password strength in colored text:
+
+```js
+const {strengthScoreTextWithColor} = require("@chaitanyareddy/simple-password-strength");
+
+const passwordStrengthColoredText = strengthScoreTextWithColor("Pass your password here!");
+console.log(passwordStrengthColoredText)
+```
+
+### Get password strength score:
+
+```js
+const {strengthScore} = require("@chaitanyareddy/simple-password-strength");
+
+const passwordScore = strengthScore("Pass your password here!");
+console.log(passwordScore)
+```
---
+++ src/index.ts
@@ -0,0 +1,58 @@
+import * as zxcvbn from 'zxcvbn';
+
+
+const getStrengthColor = (value: string) => {
+    const score = zxcvbn(value).score;
+
+    if (Number(score) === -1) return "";
+
+    if (score === 0) return "#ff0000";
+
+    if (score === 1) return "#cccc00";
+
+    if (score === 2) return "#ffa500";
+
+    if (score === 3) return "#00b300";
+
+    if (score === 4) return "#006400";
+};
+
+const getStrengthName = (value: string) => {
+    const score = zxcvbn(value).score;
+
+    if (Number(score) === -1) return "";
+
+    if (score === 0) return "too weak";
+
+    if (score === 1) return "weak";
+
+    if (score === 2) return "okay";
+
+    if (score === 3) return "good";
+
+    if (score === 4) return "strong";
+};
+
+export const strengthScore = (value: string) => {
+ if (value.length > 0) {
+            return zxcvbn(value).score;
+        } else return -1;
+}; 
+
+export const strengthScoreText = (value: string) => {
+    if (value.length > 0) {
+        return getStrengthName(value);
+    } else return -1;
+}
+
+export const strengthScoreColor = (value: string) => {
+    if (value.length > 0) {
+        return getStrengthColor(value)
+    } else return -1;
+}
+
+export const strengthScoreTextWithColor = (value: string) => {
+    if (value.length > 0) {
+        return `<span style={{color: `+ getStrengthColor(value) +`}}>` + getStrengthName(value) +`</span>`;
+    } else return -1;
+}
